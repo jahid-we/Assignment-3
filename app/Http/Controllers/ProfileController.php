8745f7fc55c0b2f5e934ff7a89e58c7d2809d9b3
Jahid Hasan <jahidhasan370919@gmail.com>
2024-06-15 01:50:21 +0600

    Add files via upload

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+        $name = "Donal Trump";
+        $age = "75";
+
+        $data = [
+            "id" => $id,
+            "name" => $name,
+            "age" => $age
+        ];
+
+        $name = "access_token";
+
+        $value = "123-XYZ";
+
+        $minutes = 1;
+
+        $path = "/";
+
+        $domain = $_SERVER["SERVER_NAME"];
+
+        $secure = false;
+
+        $httpOnly = true;
+
+        $cookie = cookie($name, $value, $minutes, $path, $domain, $secure, $httpOnly);
+
+        return response()->json($data, 200)->cookie($cookie);
+    }
+}
